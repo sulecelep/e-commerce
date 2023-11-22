5fa8f23f905e788d7dbf33ac2ae4ecef5f3a1208
ssule <ssule@CELEP>
2023-11-22 14:54:03 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import Categories from './components/Categories/Categories'
+import Footer from './components/Layout/Footer/Footer'
+import Header from './components/Layout/Header/Header'
+import Policy from './components/Layout/Policy/Policy'
+import Products from './components/Products/Products'
+import Sliders from './components/Slider/Sliders'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <Sliders/>
+      <Categories/>
+      <Products/>
+      <Policy/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
