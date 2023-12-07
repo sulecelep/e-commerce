fde63ccf8bd52a4f239f9608460df47985646022
ssule <ssulecelepp@gmail.com>
2023-12-07 21:08:26 +0300

    27 tamamlandı. React ile LocalStorage kullanımı.

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,21 +1,38 @@
-import { Fragment } from "react"
-import Footer from "../components/Layout/Footer/Footer"
-import Header from "../components/Layout/Header/Header"
-import Proptypes from "prop-types"
+import Footer from "../components/Layout/Footer/Footer";
+import Header from "../components/Layout/Header/Header";
+import Proptypes from "prop-types";
+import Search from "../components/Modals/Search/Search";
+import { useEffect, useState } from "react";
+import Dialog from "../components/Modals/Dialog/Dialog";
 
+const MainLayout = ({ children }) => {
+  const [isSearchShow , setIsSearchShow] = useState(false);
+  const [isDialogShow, setIsDialogShow]=useState(false);
+  
+  useEffect(()=>{
+    const dialogStatus = localStorage.getItem("dialog")
+    ? JSON.parse(localStorage.getItem("dialog"))
+    : localStorage.setItem("dialog",JSON.stringify(true));
+    setTimeout(()=>{
+      setIsDialogShow(dialogStatus)
+    },2000);
 
-const MainLayout = ({children}) => {
+  },[])
+ 
+  
   return (
-    <Fragment>
-        <Header/>
-        {children}
-        <Footer/>
-    </Fragment>
-  )
-}
+    <div className="main-layout">
+      <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
+      <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow} />
+      <Header setIsSearchShow ={setIsSearchShow} />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
 
-MainLayout.propTypes ={
-    children: Proptypes.node
-}
+MainLayout.propTypes = {
+  children: Proptypes.node,
+};
